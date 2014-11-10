6399a0bf62edbd59760c0569cc97d30b4bc2647a
urban <anatoly.urban@gmail.com>
2014-11-10 12:58:00 +0300

    Done in 2G, A, AG.

---
+++ common.R
@@ -0,0 +1,142 @@
+process_metabolome <- function(data, samples, names_file = NULL, result_file=NULL)
+{
+    #Collect states
+    states = unique(c(as.character(samples$state))) # biological states
+    all_states = as.character(states)
+    states = states[!(states %in% control)] # remove control from list
+    
+    if(!is.null(names_file))
+    {
+    #convert names
+        names = read.csv(names_file,sep=";", header=F, stringsAsFactors=F)
+        colnames(names) <- c("tid", "hrname")
+        names$tid <- gsub("(RI=.+),.+","\\1", names$tid)
+        data$tid <- gsub("(RI=.+),.+","\\1", data$Name)
+        data <- merge(x=data, y=names, by="tid", all.x=T, all.y=F)
+        names.found <- !is.na(data$hrname)
+        data[names.found, "Name"] = data[names.found, "hrname"]
+        data$tid <- NULL
+        data$hrname <- NULL
+    }
+    #write.csv(data, "DATA_ALL.txt")
+    
+    
+    #Auxiliary function for statistical test
+    tst <- function(row, k, e)
+    {
+        a = length(na.omit(row[c(k)])) 
+        b = length(na.omit(row[c(e)]))
+        if ( (a >= 3 & b >= 3) | (a=2 & b>=5) | (b=2 & a>=5))
+        {
+            t <- wilcox.test(as.numeric(na.omit(row[c(k)])),
+                             as.numeric(na.omit(row[c(e)])), correct = F, exact = F)
+            return(t$p.value)
+        }
+        else
+        {
+            return(1)
+        }
+    }
+    
+    Metabs <- list()  #Metabolites of interest
+    browser()
+    
+    #test each state 
+    for (s in states)
+    {
+        sk = paste0("X", samples[samples$state %in% control,"sample"]) #coltrol samples
+        se = paste0("X", samples[samples$state == s        ,"sample"]) #experiment 
+        pv <- apply(data, 1, tst, sk, se)
+        pv[is.na(pv)] <- 1
+        Metabs <- unique(c(Metabs, as.character(data[pv < PV_REQ, "Name"])))
+        
+    }
+
+    #consruct dataframe with fold changes for each found Metabolite 
+    dh = data.frame(Name = data$Name) #data for heatmap
+    for (s in all_states)
+    {
+        se = paste0("X", samples[samples$state == s, "sample"]) #experiment samples
+        dh = cbind(dh, apply(data[,se], 1, mean))
+    }
+    names(dh) = c("Name", all_states)
+    rownames(dh) <- dh$Name
+    
+    dh$Name <- NULL
+    
+    ############################################
+    ##Zero control workaround
+    #dh[dh[control]==0, control] <- ZEROCOUNT 
+    #dh <- dh+ZEROCOUNT
+    
+    ##Somethind bad is going here: you need to fix it!
+    mn = min(dh[dh>0])
+    mx = max(dh[dh>0])
+    
+    DF <- function(x,y){
+        r = x/y
+        r[x==0 & y==0] <- 1
+        r[x!=0 & y==0] <- mx/mn
+        r
+    }
+    
+    dh = as.data.frame(sweep(dh, 1, dh[,control], FUN=DF)) #normalize to control)
+    ############################################
+    
+    dh.raw = dh
+    dh = dh[rownames(dh) %in% Metabs,]
+    dh = log2(dh)
+    dh[,control] = NULL
+    
+    require(ggplot2)
+    require(reshape2)
+    require(RColorBrewer)
+    
+    dm = as.matrix(dh)
+    dm.m = melt(dm)
+    
+    #handle infinites
+    t = dm.m$value
+    t = t[is.finite(t)]
+    mm = max(abs(t))
+    dinf = is.infinite(dm.m$value)
+    dm.m$value[dinf] <- mm*sign(dm.m$value[dinf])
+    
+    # NAAAAAA
+    dm.m = dm.m[!is.na(dm.m$value),]
+    
+    if(!is.null(result_file))
+    {    
+        write.csv(dh.raw, result_file)
+    }
+    
+    
+    return (dm.m)
+}
+
+draw_heatmap <- function(dm, label, filename=NULL, palette=NULL)
+{
+    if (is.null(palette))
+    {
+        #COLOR palletes setut is here
+        myPalette <- colorRampPalette(rev(brewer.pal(5, "RdBu")), space="rgb")
+        #myPalette <- colorRampPalette(rev(c("red", "white","blue")))
+    }
+    mm = max(abs(dm$value))
+    p <- ggplot(dm, aes(x=Var2, y=Var1, fill=value))
+    p <- p + geom_tile()
+    p <- p + scale_fill_gradientn(colours = myPalette(20), 
+                                  name=label, limits=c(-mm,mm))
+    p <- p + xlab("Treatment") + ylab("Metabolite")
+    p <- p + scale_x_discrete(expand = c(0, 0))
+    p <- p + scale_y_discrete(expand = c(0, 0))
+    p <- p + coord_equal()
+    p <- p + theme_bw()
+    p <- p + theme(axis.text.x=element_text(angle=45, hjust=1))
+    if (!is.null(filename))
+    {
+        ggsave(heatmap.file)
+        dev.off()
+    }
+    print(p)
+}
--- mossAG.R
+++ mossAG.R
@@ -1,4 +1,6 @@
-setwd(".")
+rm(list=ls())
+source("common.R")
+
 data.file1 = "./data/moss1_data.txt"
 samples.file1 = "./data/moss1_samples.txt"
 data.file2 = "./data/moss2_data.txt"
@@ -16,7 +18,6 @@ colnames(data1) = gsub("X", "X1_", colnames(data1))
 samples1 = read.csv(samples.file1, header=T)
 samples1$sample = paste0("1_", samples1$sample)
 
-
 #second set of data
 data2 = read.csv(data.file2, header=T, stringsAsFactors=F)
 data2[is.na(data2)] <- 0 # empty cell => no metabolite
@@ -24,140 +25,9 @@ colnames(data2) = gsub("X", "X2_", colnames(data2))
 samples2 = read.csv(samples.file2, header=T)
 samples2$sample = paste0("2_", samples2$sample)
 
-#Collect states
-states = unique(c(as.character(samples1$state),
-                  as.character(samples2$state))) # biological states
-all_states = as.character(states)
-states = states[!(states %in% control)] # remove control from list
-
 samples = rbind(samples1, samples2)
 data = merge(x=data1, by.x="Name", y=data2, by.y="Name", all=TRUE)
 
-#convert names
-names = read.csv(names.file,sep=";", header=F, stringsAsFactors=F)
-colnames(names) <- c("tid", "hrname")
-names$tid <- gsub("(RI=.+),.+","\\1", names$tid)
-data$tid <- gsub("(RI=.+),.+","\\1", data$Name)
-data <- merge(x=data, y=names, by="tid", all.x=T, all.y=F)
-names.found <- !is.na(data$hrname)
-data[names.found, "Name"] = data[names.found, "hrname"]
-data$tid <- NULL
-data$hrname <- NULL
+dm.m <- process_metabolome(data, samples, names.file, result.file)
 
-#write.csv(data, "DATA_ALL.txt")
-
-
-#Auxiliary function for statistical test
-tst <- function(row, k, e)
-{
-    if ( length(na.omit(row[c(k)])) < 3 |  length(na.omit(row[c(e)])) < 3)
-    {
-        return(1)
-    }
-    t <- wilcox.test(as.numeric(na.omit(row[c(k)])),
-                     as.numeric(na.omit(row[c(e)])), correct = F, exact = F)
-    t$p.value
-}
-
-Metabs <- list()  #Metabolites of interest
-
-#test each state 
-for (s in states)
-{
-    sk = paste0("X", samples[samples$state %in% control,"sample"]) #coltrol samples
-    se = paste0("X", samples[samples$state == s        ,"sample"]) #experiment samples
-#     d  = data.frame(Name = data$Name, data[,sk], data[,se])
-#     d$pv <- apply(d, 1, tst, sk, se) # test
-#     d[is.na(d$pv), "pv"] <- 1
-#     Metabs <- unique(c(Metabs, as.character(d[d$pv < PV_REQ, "Name"])))
-    pv <- apply(data, 1, tst, sk, se)
-    pv[is.na(pv)] <- 1
-    Metabs <- unique(c(Metabs, as.character(data[pv < PV_REQ, "Name"])))
-    
-}
-
-# d = data[data$Name %in% Metabs,]
-# dh = data.frame(Name = d$Name) #data for heatmap
-# for (s in all_states)
-# {
-#     se = paste0("X", samples[samples$state == s, "sample"]) #experiment samples
-#     dh = cbind(dh, apply(d[,se], 1, mean))
-# }
-# names(dh) = c("Name", all_states)
-# rownames(dh) <- dh$Name
-# dh$Name <- NULL
-
-#consruct dataframe with fold changes for each found Metabolite 
-dh = data.frame(Name = data$Name) #data for heatmap
-for (s in all_states)
-{
-    se = paste0("X", samples[samples$state == s, "sample"]) #experiment samples
-    dh = cbind(dh, apply(data[,se], 1, mean))
-}
-names(dh) = c("Name", all_states)
-rownames(dh) <- dh$Name
-
-dh$Name <- NULL
-
-############################################
-##Zero control workaround
-#dh[dh[control]==0, control] <- ZEROCOUNT 
-#dh <- dh+ZEROCOUNT
-
-##Somethind bad is going here: you need to fix it!
-mn = min(dh[dh>0])
-mx = max(dh[dh>0])
-
-DF <- function(x,y){
-    r = x/y
-    r[x==0 & y==0] <- 1
-    r[x!=0 & y==0] <- mx/mn
-    r
-}
-
-dh = as.data.frame(sweep(dh, 1, dh[,control], FUN=DF)) #normalize to control)
-############################################
-
-dh.raw = dh
-dh = dh[rownames(dh) %in% Metabs,]
-dh = log2(dh)
-dh[,control] = NULL
-
-require(ggplot2)
-require(reshape2)
-require(RColorBrewer)
-
-dm = as.matrix(dh)
-dm.m = melt(dm)
-
-#handle infinites
-t = dm.m$value
-t = t[is.finite(t)]
-mm = max(abs(t))
-dinf = is.infinite(dm.m$value)
-dm.m$value[dinf] <- mm*sign(dm.m$value[dinf])
-
-# NAAAAAA
-dm.m = dm.m[!is.na(dm.m$value),]
-
-#COLOR palletes setut is here
-#myPalette <- colorRampPalette(rev(brewer.pal(10, "Spectral")), space="Lab")
-myPalette <- colorRampPalette(rev(brewer.pal(5, "RdBu")), space="rgb")
-#myPalette <- colorRampPalette(rev(c("red", "white","blue")))
-
-p <- ggplot(dm.m, aes(x=Var2, y=Var1, fill=value))
-p <- p + geom_tile()
-p <- p + scale_fill_gradientn(colours = myPalette(20), 
-                              name=expression(log[10](fc)), limits=c(-mm,mm))
-p <- p + xlab("Treatment") + ylab("Metabolite")
-p <- p + scale_x_discrete(expand = c(0, 0))
-p <- p + scale_y_discrete(expand = c(0, 0))
-p <- p + coord_equal()
-p <- p + theme_bw()
-p <- p + theme(axis.text.x=element_text(angle=45, hjust=1, size=8))
-ggsave(heatmap.file)
-dev.off()
-print(p)
-write.csv(dh.raw, result.file)
-
-#ggsave(heatmap.file)
+draw_heatmap(dm.m, expression(log[10](fc)), heatmap.file)
