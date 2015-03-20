5cd6342738bb0c909a0664bff4df7b9984cdd5c9
urban <anatoly.urban@gmail.com>
2015-03-20 20:07:52 +0300

    New process for d3.

--- moss2015.R
+++ moss2015.R
@@ -21,18 +21,27 @@ data$FileName <- NULL
 data$Name <- sub("^[?[:space:]]*", "", data$Name)
 d1 <- data[data$sample %in% 1:36,]
 d2 <- data[data$sample %in% 37:72,]
+d3 <- data[data$sample %in% 81:106,]
+
 d1 <- dcast(d1, Name ~ sample, value.var = "Conc.")
 d2 <- dcast(d2, Name ~ sample, value.var = "Conc.")
+d3 <- dcast(d3, Name ~ sample, value.var = "Conc.")
 
 names(d1) <- paste0("X", names(d1)) # Compatibility with old code
 names(d1)[1] <- "Name"
 d1[is.na(d1)] <- 0 # empty cell => no metabolite
+#d1[d1==""] <- 0
 names(d2) <- paste0("X", names(d2)) # Compatibility with old code
 names(d2)[1] <- "Name"
 d2[is.na(d2)] <- 0 # empty cell => no metabolite
+#d2[d2==""] <- 0
+names(d3) <- paste0("X", names(d3)) # Compatibility with old code
+names(d3)[1] <- "Name"
+d3[is.na(d3)] <- 0 # empty cell => no metabolite
+#d3[d3==""] <- 0
 
 d3 <- read.csv(data.file.old, header=T, stringsAsFactors=F)
-d3[is.na(d3)] <- 0 # empty cell => no metabolite
+#d3[is.na(d3)] <- 0 # empty cell => no metabolite
 
 samples <- read.csv(samples.file, header=T, stringsAsFactors=F)
 rownames(samples) <- samples$sample
